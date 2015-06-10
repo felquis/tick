11b10564ccb65a55b0db515c87d013b8a9146440
Felquis Gimenes <frgformenton@gmail.com>
2015-06-10 12:07:21 -0300

    Create canvas

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
-var Program = function (width, height, init, outputElement) {
-	this.width = width || 50
-	this.height = height || 50
-	this.outputElement = outputElement
+var Program = function (width, height, init, canvas) {
+	this.width = width
+	this.height = height
 	this.generation = 0
 	this.velocity = 1000
 	this.timeouts = []
@@ -9,61 +8,63 @@ var Program = function (width, height, init, outputElement) {
 	this.compare1 = 0
 	this.compare2 = 1
 
-	this.generateMatrix()
+	this.canvas = canvas
+	this.context = canvas.getContext('2d')
+
+	this.canvas.width = this.width
+	this.canvas.height = this.height
+
+	this.imageData = this.context.getImageData(this.canvas.width, this.canvas.height, 500, 500)
+	this.length = this.imageData.data.length
+	this.data = this.imageData.data
+
+	this.nextTick()
 }
 
 Program.prototype.nextTick = function () {
-	var self = this
+	console.log('nextTick')
+	for (var i = 0; i < this.length; i += 4) {
+		/*
+			Lógica
 
-	this.generation += 1
+			-: data[i + 1] = 0;
+			+: data[i + 1] = 1;
 
-	var newArray = this.data
-	var self = this
-	var y
-	var x
-	var yTotal = this.height
-	var xTotal = this.width
+			data[i] = 1; === data[i] = 1; = +
+			data[i] = 0; === data[i] = 0; = +
+			data[i] = 0; === data[i] = 1; = -
+			data[i] = 1; === data[i] = 0; = -
+		*/
 
-	this.data.forEach(function (row, y) {
-		for (x = 0; x < xTotal; x++) {
-			newArray[y][x + 1] = (newArray[y][x - self.compare1] === newArray[y][x - self.compare2])? 1 : 0
-		}
-	})
+	  if (this.data[i - this.compare1] == this.data[i - this.compare2]) {
+	  	this.data[i + (0)] = 1
+	  } else {
+	  	this.data[i + (0)] = 0
+	  }
 
-	this.data = newArray
-}
+	  if (this.data[i]) {
+			this.data[i + 1] = 250;
+		  this.data[i + 2] = 0;
+		  this.data[i + 3] = 250;
+	  } else {
+	  	this.data[i + 1] = 0;
+		  this.data[i + 2] = 250;
+		  this.data[i + 3] = 250;
+	  }
+	}
 
-Program.prototype.render = function () {
-	this.outputElement.innerHTML = ''
-
-	var fragment = document.createDocumentFragment()
-
-	this.data.forEach(function (yValue, yIndex) {
-		var line = document.createElement('div')
-		line.classList.add('row')
-
-		yValue.forEach(function (xValue, xIndex) {
-			var span = document.createElement('span')
-			span.classList.add('row-item')
-			span.classList.add('row-item-' + xValue)
-			span.innerText = xValue
-			line.appendChild(span)
-		})
-
-		fragment.appendChild(line)
-	})
-
-	this.outputElement.appendChild(fragment)
+	this.context.putImageData(this.imageData, 0, 0)
 }
 
 Program.prototype.play = function () {
+	console.log('play')
 
 	var self = this
 
 	self.playing = 1
 
 	var id = setTimeout(function () {
-		self.renderNextTick()
+		self.nextTick()
 
 		if (self.playing) {
 			self.timeouts = []
@@ -75,6 +76,7 @@ Program.prototype.play = function () {
 }
 
 Program.prototype.pause = function () {
+	console.log('pause')
 	this.playing = 0
 
 	this.timeouts.forEach(function (id) {
@@ -82,37 +84,31 @@ Program.prototype.pause = function () {
 	})
 }
 
+// Init program
+var program = new Program(500, 500, 0, document.querySelector('#canvas'))
+program.compare2 = 4
+program.play()
 
-Program.prototype.renderNextTick = function () {
-	this.nextTick();
-	this.render()
-}
+// Controles
+document.addEventListener('input', function (event) {
+	var output = document.querySelector('[for="' + event.target.id + '"]')
 
-Program.prototype.generateMatrix = function () {
+	if (output) {
+		output.innerHTML = event.target.value
+	}
 
-	var array = []
-	var self = this
-	var y
-	var x
-	var yTotal = this.height
-	var xTotal = this.width
+	program[event.target.dataset.set] = parseInt(event.target.value)
 
-	for (y = 0; y < yTotal; y++) {
-		array[y] = []
+	console.log(program[event.target.dataset.set])
+})
 
-		for (x = 0; x < xTotal; x++) {
+// Controles
+document.addEventListener('click', function (event) {
+	var fun = program[event.target.dataset.action]
 
-			if (x === 0) {
-				array[y][x] = y % 2
-			} else {
-				array[y][x + 1] = (!!array[y][x] === !!array[y][x - 1])? 1 : 0
-			}
-		}
-	};
+	if (typeof fun === 'function') {
+		program[event.target.dataset.action]()
+	}
+})
 
-	this.data = array
-}
 
-var program = new Program(50, 50, 0, document.querySelector('.container'))
-
-program.renderNextTick()
