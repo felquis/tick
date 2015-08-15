cb94318cedbfd4dc684979718ca9f5727a471e38
Felquis Gimenes <frgformenton@gmail.com>
2015-08-14 23:21:44 -0300

    Revert "Interlucidativo cara"
    This reverts commit 60998510c7c000f8652292f67149a900c2f43589.

--- src/js/main.js
+++ src/js/main.js
@@ -1,114 +1,166 @@
-(function () {
-  function Program() {
+var Program = function (width, height, init, outputElement) {
+	this.width = width || 50
+	this.height = height || 50
+	this.outputElement = outputElement
+	this.generation = 0
+	this.velocity = 1000
+	this.timeouts = []
+	this.startWith = init
+	this.compare1 = 4
+	this.compare2 = 2
+	this.data = []
 
-    return this;
-  }
+	this.generateMatrix()
+}
 
-  Program.prototype.createLine = createLine;
+Program.prototype.nextTick = function () {
+	var self = this
 
-  function guessWhatsNext(){
+	this.generation += 1
 
-  }
+	var self = this
+	var array = []
+	var y
+	var x
+	var yTotal = this.height
+	var xTotal = this.width
+	var total = self.height * self.width
+	var i = 0
+	var currentHeight = 0
 
-  function createLine() {
-    var colunas = 3;
-    var i = 0;
-    var previous = [[1,0,1]];
-    var result = [];
-    var iResult;
+	for (i; i < total; i++) {
+		if ((i % self.width) !== 0 && i !== 0) {
+			array[currentHeight].push((i % self.compare1) % self.compare1)
+		} else if ((i % self.width === 0)) {
+			currentHeight += 1
+			array[currentHeight] = []
+			array[currentHeight].push((i % self.compare1) % self.compare2)
+		}
+	};
 
-    /*
-      Quando estou no 1
-      Pego o 0, e comparo com o 1
-      O resultado é o 2
+	this.data = array
+}
 
-      Sei la o que é isso
-      +++
-      +-+
-      -+?
-      +++
+Program.prototype.render = function () {
+	this.outputElement.innerHTML = ''
 
-      Alguns padrões (já que estou com paciência)
+	var fragment = document.createDocumentFragment()
 
-      A inverção do primeiro valor da linha acima
-      -+--+--+--+--+--+
-      +--+--+--+--+--+-
-      --+--+--+--+--+--
-      +--+--+--+--+--+-
-      --+--+--+--+--+--
-      +--+--+--+--+--+-
+	var self = this
 
-      -+-
-      +--
-      --+
-      +--
-      --+
+	this.data.forEach(function (yValue, yIndex) {
+		var line = document.createElement('div')
+		line.classList.add('row')
 
-      -+
-      +-
+		yValue.forEach(function (xValue, xIndex) {
+			var r = (yIndex + 1) * (xIndex + 1)
+			var g = r % self.compare1 % self.compare2 * r
+			var b = r % self.compare1 % self.compare2 * r
 
-      O espelho da linha acima
-      -+--+--+--+--+--+
-      +-++-++-++-++-++-
-      -+--+--+--+--+--+
-      +-++-++-++-++-++-
-      -+--+--+--+--+--+
-      +-++-++-++-++-++-
-      -+--+--+--+--+--+
-      +-++-++-++-++-++-
+			var span = document.createElement('span')
+			span.classList.add('row-item')
+			span.classList.add('row-item-' + xValue)
+			span.style.background = 'rgb('+r+', '+g+', '+b+')';
+			span.innerText = xValue
+			line.appendChild(span)
+		})
 
-      Triangulo?
-      -+--+--+--+--+--+-
-       --+--+--+--+--+--
-        +--+--+--+--+--+
-         -+--+--+--+--+-
-          --+--+--+--+--
-           +--+--+--+--+
-            -+--+--+--+-
-             --+--+--+--
-              +--+--+--+
-               -+--+--+-
-                --+--+--
-                 +--+--+
-                  -+--+-
-                   --+--
-                    --+-
-                     +--
-                      -+
-                       -
-                      -+
-                     --+
-                    ????
-                   ?????
-                  ??????
-                 ???????
-                ????????
-               ?????????
-              ??????????
-             ???????????
-            ????????????
-           ?????????????
-          ??????????????
-         ???????????????
-        ????????????????
-       ?????????????????
-      ??????????????????
-    */
+		fragment.appendChild(line)
+	})
 
-    for (i; i < colunas; i++) {
-      iResult = !!result[i - 1] === !!result[i];
+	this.outputElement.appendChild(fragment)
+}
 
-      result.push(iResult);
-    };
+Program.prototype.play = function () {
 
-    console.log(result);
+	var self = this
 
-    debugger
-  }
+	self.playing = 1
 
-  window.Program = Program;
-}())
+	var id = setTimeout(function () {
+		self.renderNextTick()
 
-var run = new Program();
+		if (self.playing) {
+			self.timeouts = []
+			self.play()
+		}
+	}, self.velocity)
 
-run.createLine();
+	self.timeouts.push(id)
+}
+
+Program.prototype.pause = function () {
+	this.playing = 0
+
+	this.timeouts.forEach(function (id) {
+		clearTimeout(id)
+	})
+}
+
+
+Program.prototype.renderNextTick = function () {
+	this.nextTick();
+	this.render()
+}
+
+Program.prototype.generateMatrix = function () {
+
+	var self = this
+	var array = []
+	var y
+	var x
+	var yTotal = this.height
+	var xTotal = this.width
+	var total = self.height * self.width
+	var i = 0
+	var currentHeight = 0
+
+	for (i; i < total; i++) {
+		if ((i % self.width) !== 0 && i !== 0) {
+			array[currentHeight].push((i % self.compare1) % self.compare1)
+
+			// console.log(currentHeight, i, array[currentHeight][i % self.width])
+
+			// console.log('Adiciona item', currentHeight, i, (i % 3) % 2)
+		} else if ((i % self.width === 0)) {
+			currentHeight += 1
+			array[currentHeight] = []
+			array[currentHeight].push((i % self.compare1) % self.compare2)
+
+			// console.log(i, array[currentHeight][i % self.width])
+			// console.log('Add row', currentHeight, i, )
+		}
+	};
+
+	this.data = array
+}
+
+var program = new Program(10, 10, 1, document.querySelector('.container'))
+program.compare2 = 1
+program.render()
+
+program.velocity = 500
+program.play()
+
+// Controles
+document.addEventListener('input', function (event) {
+	var output = document.querySelector('[for="' + event.target.id + '"]')
+
+	if (output) {
+		output.innerHTML = event.target.value
+	}
+
+	program[event.target.dataset.set] = parseInt(event.target.value)
+
+	console.log(program[event.target.dataset.set])
+})
+
+// Controles
+document.addEventListener('click', function (event) {
+	var fun = program[event.target.dataset.action]
+
+	if (typeof fun === 'function') {
+		program[event.target.dataset.action]()
+	}
+})
+10
